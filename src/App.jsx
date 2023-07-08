bf338aabb8879ab00a83a456cf52cf7cc5163089
Gümrah Sindar <gsindar@gmail.com>
2023-07-08 13:51:21 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Features from './comps/Features'
+import Hero from './comps/Hero'
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Features />
+    </>
+  )
+}
+
+export default App
